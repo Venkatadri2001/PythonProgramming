f2c2c099d864213d898c562af60cfc4d5e4758ff
Venkatadri Majjari <142674720+Venkatadri2001@users.noreply.github.com>
2023-12-13 22:37:16 +0530

    Add files via upload

---
+++ LogicalOperators.py
@@ -0,0 +1,28 @@
+print(True and True)  # logical and 
+
+print(True and False) # logical and
+
+print(False and False) # logical and
+
+print(False and True) # logical and
+
+print(True or True)  # logical or
+
+print(True or False) # logical or
+
+print(False or False) # logical or
+
+print(False or True) # logical or
+
+print(not True)  # logical not  (viseversa)
+print()
+
+# example
+
+a=3>0
+b=3<0
+print(a and b) # True and False = false
+
+print(a or b) # True or false = true
+
+print(not b) # not false = true
