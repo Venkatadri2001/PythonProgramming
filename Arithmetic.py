f7f29a57a477b24bfe4a5d7de704c5510aca2485
Venkatadri Majjari <142674720+Venkatadri2001@users.noreply.github.com>
2023-12-13 15:03:21 +0530

    Add files via upload

---
+++ Arithmetic.py
@@ -0,0 +1,23 @@
+a = int(input("Enter First Number:"))
+b = int(input("Enter Second Number:"))
+
+# addition
+print ('Sum: ', a + b)  
+
+# subtraction
+print ('Subtraction: ', a - b)   
+
+# multiplication
+print ('Multiplication: ', a * b)  
+
+# division
+print ('Division: ', a / b) 
+
+# floor division
+print ('Floor Division: ', a // b)
+
+# modulo
+print ('Modulo: ', a % b)  
+
+# a to the power b
+print ('Power: ', a ** b)   
