0576954a9ea8c63bbbf8ba8045963193394c3f00
Venkatadri Majjari <142674720+Venkatadri2001@users.noreply.github.com>
2023-12-13 15:27:40 +0530

    Add files via upload

---
+++ AssignmentOperators.py
@@ -0,0 +1,41 @@
+a=3
+b=4
+
+a = b # assignment opr
+print(a)
+
+a += b # addition assignment
+print(a)
+
+a -= b # subtraction assignment
+print(a)
+
+a *= b # multiplication assignment
+print(a)
+
+a /= b # division assignment
+print(a)
+
+a %= b # remainder assignment
+print(a)
+
+a **= b # Exponent assignment
+print(a)
+
+a //= b 
+print(a)
+
+# a &= b 
+# print(a)
+
+# a |= b 
+# print(a)
+
+# a >>= b 
+# print(a)
+
+# a <<= b 
+# print(a)
+
+# a ^= b 
+# print(a)
---
+++ RelationalOperators.py
@@ -0,0 +1,20 @@
+a = int(input("Enter the First Num:"))
+
+b = int(input("Enter the Second Num:"))
+# equal to operator
+print('a == b', a == b)
+
+# not equal to operator
+print('a != b =', a != b)
+
+# greater than operator
+print('a > b =', a > b)
+
+# less than operator
+print('a < b =', a < b)
+
+# greater than or equal to operator
+print('a >= b =', a >= b)
+
+# less than or equal to operator
+print('a <= b =', a <= b)
